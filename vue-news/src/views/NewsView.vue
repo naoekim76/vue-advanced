c47e66541a94f0313590ae4c729bcb3d0176d6d3
jhkim <hansalim76@gmail.com>
2022-03-16 08:42:05 +0900

    axios 이용한 api 호출

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    News
+    <div v-for="user in users" :key="user">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    var vm = this;
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(function(response) {
+        console.log(response);
+        vm.users = response.data;
+      })
+      .catch(function() {
+
+      })
+  }
 
 }
 </script>
