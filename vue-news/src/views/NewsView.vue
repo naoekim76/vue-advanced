220d1ffa8b34e2fca9c715cc1a9e932de69c5689
jhkim <hansalim76@gmail.com>
2022-03-16 08:56:45 +0900

    axios의 api 함수 구조화 방법 및 실습

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div v-for="user in users" :key="user">{{ user.title }}</div>
+    <div v-for="user in users" :key="user.id">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import {fetchNewsList} from '../api/index.js';
 export default {
   data() {
     return {
@@ -14,7 +14,7 @@ export default {
   },
   created() {
     var vm = this;
-    axios.get('https://api.hnpwa.com/v0/news/1.json')
+    fetchNewsList()
       .then(function(response) {
         console.log(response);
         vm.users = response.data;
